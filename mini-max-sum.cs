cb0a781660c02bc41549626849551d51c05edd1c
Michael Rozendo <79727570+mikerozendo@users.noreply.github.com>
2022-04-15 15:02:33 -0300

    Try catch statement
    Adding try catch to handly work with OverFlowException in System.Linq.Sum()

--- mini-max-sum.cs
+++ mini-max-sum.cs
@@ -8,13 +8,40 @@ class Result
 {
     public static void miniMaxSum(List<int> arr)
     {
-        if (arr.Count > 0)
+        if (arr.Count == 5)
         {
-            int min = arr.Min();
-            int max = arr.Max();
+            long min = arr.Min();
+            long max = arr.Max();
+            Int64 minSum = 0;
+            Int64 maxSum = 0;
 
-            int minSum = (from value in arr where value < max select value).Sum();
-            int maxSum = (from value in arr where value > min select value).Sum();
+
+            try
+            {
+                minSum = (from value in arr where value < max select value).Sum();
+                maxSum = (from value in arr where value > min select value).Sum();
+
+                if (maxSum == 0 && minSum == 0)
+                {
+                    arr.RemoveAt(0);
+                    minSum = arr.Sum();
+                    maxSum = arr.Sum();
+                }
+            }
+            catch (OverflowException)
+            {
+                var minList = (from value in arr where value < max select value).ToList();
+                var maxList = (from value in arr where value > min select value).ToList();
+
+                minSum = 0;
+                maxSum = 0;
+
+                foreach (int value in minList)
+                    minSum += value;
+
+                foreach (int value in maxList)
+                    maxSum += value;
+            }
 
             Console.WriteLine($"{minSum} {maxSum}");
         }
